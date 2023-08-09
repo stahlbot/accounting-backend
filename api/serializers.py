7c284db134bb7b079baf2607d9d5c45a45ada65f
Timon Stahlbock <stahlbot@uni-bremen.de>
2023-08-09 21:46:25 +0200

    return username and id on authorization

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,32 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainSerializer, RefreshToken
+from rest_framework_simplejwt.settings import api_settings
+from django.contrib.auth.models import update_last_login
+
+# from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .models import User
+
+# Overwrite simple jwt ObtainPairSerializer to include the id and name of the user after authentication
+class MyTokenObtainPairSerializer(TokenObtainSerializer):
+    token_class = RefreshToken
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        # include name and id
+        data['username'] = self.user.username
+        data['id'] = self.user.id
+
+        return data
 
 
 class UserSerializer(serializers.ModelSerializer):
